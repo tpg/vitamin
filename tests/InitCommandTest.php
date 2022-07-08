8044eaa7a07e8f42190bd899530e1aaf190c6860
Warrick <me@warrickbayman.com>
2022-07-08 12:41:42 +0200

    update tests.

--- tests/InitCommandTest.php
+++ tests/InitCommandTest.php
@@ -42,8 +42,6 @@ class InitCommandTest extends TestCase
         $this->artisan(InitCommand::class)
             ->expectsConfirmation('Are you sure you want to continue?', 'yes')
             ->expectsQuestion('What hostname are you using in development? (e.g.: valet.test):', 'vitamin.test')
-            ->expectsQuestion('Where are your JS sources stored? (relative to resources directory)', 'js')
-            ->expectsQuestion('Where are your Inertia Vue pages stored? (relative to resources directory)', 'js/Pages')
             ->expectsChoice('Which Node dependency manager do you use?', 'yarn', ['yarn', 'npm'])
             ->assertExitCode(0);
     }
